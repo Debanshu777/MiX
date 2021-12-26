dfb0ac2127188ccc88a44017d6ef46356b8e9ead
Debanshu Datta <debanshudatta123@gmail.com>
2021-12-26 10:51:47 +0530

    KtLint Formatting

--- app/src/main/java/com/debanshu777/mix/MiXApplication.kt
+++ app/src/main/java/com/debanshu777/mix/MiXApplication.kt
@@ -3,7 +3,7 @@ package com.debanshu777.mix
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 
-class MiXApplication: Application() {
+class MiXApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Apply dynamic color
